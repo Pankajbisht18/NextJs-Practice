3eca6ef672740946b69d3cb96275c4edb960ea4e
Pankajbisht18 <techpankaj45@gmail.com>
2021-12-03 23:26:42 +0530

    Add Navbar and layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,18 @@
+import Nav from './Nav'
+import Header from './Header'
 import styles from '../styles/Layout.module.css'
 
-const Layout = () => {
+const Layout = ({children}) => {
     return (
-        <div className={styles.container}>
-            
-        </div>
+        <>
+            <Nav />
+            <Header />
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </>
     )
 }
 export default Layout;
